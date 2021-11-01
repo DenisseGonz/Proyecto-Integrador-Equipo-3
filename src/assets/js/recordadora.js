2df4df35f8fca1f51661710f76c90f2dc7d6ae84
Sheila Sanchez Ramirez <sanchezramirezsheila@gmail.com>
2021-11-01 09:49:15 -0600

    Vistas responsive completas

---
+++ src/assets/js/recordadora.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Sweet!',
+    text: 'Modal with a custom image.',
+    imageUrl: 'https://unsplash.it/400/200',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })

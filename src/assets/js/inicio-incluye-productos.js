a5a0f76c9b6b4070adde86f04ad42bc67f59cd43
Sergio Borjon E <sergioborjon1@gmail.com>
2021-11-02 17:31:49 -0600

    Integracion de la vista detallada de productos

--- src/assets/js/inicio-incluye-productos.js
+++ src/assets/js/inicio-incluye-productos.js
@@ -9,22 +9,25 @@ con ese atributo o simplemente quitarlo para que todos se agreguen
 
 /*En caso de querer usar este script es posible que necesite crear un div con id #productos
 o en su defecto cambiar estas lineas apropiadamente por el id de su página*/
+import { max } from "lodash";
+import Swal from "sweetalert2";
 export function agprodini(){
 const $contenedor = document.querySelector("#productos");
 const $carrusel = document.querySelector("#carrusel");
 
-fetch("https://denissegonz.github.io/Proyecto-Integrador-Equipo-3/src/productos.json")
+fetch("../src/productos.json")
     .then(resp => resp.json())
     .then(data => {
         var j=0;
         data.forEach(i => {
             if (i.destacado == "si") {
                 let $singleProduct = document.createElement("div");
+                var id=`img${j+1}`;
                 $singleProduct.className = "producto col-md-6 col-lg-4 col-xl-3";
                 var a = `<div class="producto__parte-1">
-                <img class="producto__parte-1--img" src="${i.img}">`
+                        <img id="${id}" class="producto__parte-1--img" src="${i.img}">`
                 if (i.descuento != " ") {
-                    a = a + `<span class="producto__parte-1--descuento">${i.descuento}</span>`
+                    a = a +`<span class="producto__parte-1--descuento">${i.descuento}</span>`
                 }
                 if (i.tipo != " ") {
                     a = a + `<span class="producto__parte-1--nuevo">${i.tipo}</span>`
@@ -38,10 +41,12 @@ fetch("https://denissegonz.github.io/Proyecto-Integrador-Equipo-3/src/productos.
                 if (i.antiguoPrecio !== " ") {
                     a = a + `<h4 class="producto__parte-2--viejo-precio">${i.antiguoPrecio}</h4>`
                 }
-                a = a + `<h4 class="producto__parte-2--precio">${i.precio}</h4></div>
-                <ul class="producto__parte-2--ul">
-                <li><a href="#" class="chest"></a></li>
-                </ul>   `/*Agregare esta linea para probar una cuestion con el cofre*/
+                a = a +`<h4 class="producto__parte-2--precio">${i.precio}</h4></br>
+                <span class="btn producto__parte-2--boton">
+                    <a href="#">Agregar al baúl</a>
+                </span>
+                </div>`
+                /*Agregare esta linea para probar una cuestion con el cofre*/
                 $singleProduct.innerHTML = a
                 $contenedor.appendChild($singleProduct);
                 /*esta parte del codigo solo agrega el carrusel de la vista móvil puede ignorarse*/
@@ -54,6 +59,24 @@ fetch("https://denissegonz.github.io/Proyecto-Integrador-Equipo-3/src/productos.
                 $caritem.innerHTML=a;
                 $carrusel.appendChild($caritem);
                 j++;
+                /*esta parte del codigo genera las vistas detalladas*/
+                let $img=document.querySelector(`#${id}`);
+                $img.addEventListener("click",()=>{
+                    Swal.fire({
+                        html:
+                        `<img class="producto__parte-1--img-pop" src=${i.img}>
+                        <h3 class="producto__parte-2--titulo-des">${i.nombre}</h3>
+                         <h4 class="producto__parte-2--precioDes">${i.precio}</h4>
+                         <h4 class="producto__parte-2--descripcion">${i.descripcion}</h4>`,
+                        showCloseButton:true,
+                        confirmButtonText:"Agregar al baúl",
+                        confirmButtonColor:'#9f8932',
+                        showCancelButton:true,
+                        cancelButtonColor: '#363859',
+                        cancelButtonText:"regresar",
+                        background: 'rgba(0,0,0,0.8)',
+                    })
+                }) 
             }
         });
     })
@@ -61,3 +84,9 @@ fetch("https://denissegonz.github.io/Proyecto-Integrador-Equipo-3/src/productos.
         console.log(e);
     });
 }
+
+
+
+
+
+
